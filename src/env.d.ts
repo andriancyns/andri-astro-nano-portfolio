53a085eb75053d30521385c14bff6b310806f908
Andrianto Cahyono Putro <andri.c.putro@gmail.com>
2025-08-09 14:48:56 +0700

    refactor: update configs and component scripts for better typing
    add type definitions for environment variables and modules
    update tsconfig to include astro client types and jsx preservation
    use is:inline attribute for giscus script loading
    remove unused SITE import from PageLayout

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,14 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+/// <reference types="@astrojs/image/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_GC_PROJECT_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+declare module '*.woff2';
+declare module 'astro:transitions';

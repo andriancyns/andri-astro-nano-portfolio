11ec19c210d660cc46d8f2d27f82a9e4634b4344
Andrianto Cahyono Putro <andri.c.putro@gmail.com>
2025-08-19 13:14:29 +0700

    fix: update site URL in astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://cyns.space",
   integrations: [mdx(), sitemap(), tailwind()],
 });

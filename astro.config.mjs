2a02d393eb9638a371aa51665f3a1af230cef195
Andrianto Cahyono Putro <andri.c.putro@gmail.com>
2025-12-29 17:33:03 +0700

    feat: Add Netlify adapter and remove Keystatic API and UI files.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import keystatic from "@keystatic/astro";
+import netlify from "@astrojs/netlify";
 
 export default defineConfig({
   site: "https://cyns.space",
   output: "hybrid",
+  adapter: netlify(),
   integrations: [mdx(), sitemap(), tailwind(), react(), keystatic()],
 });
